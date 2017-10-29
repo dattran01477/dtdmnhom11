7155ca00d88e0ac1346c6140f3dfa5c3fede9bae
hquanitute <hquanitute@gmail.com>
2017-10-29 15:42:52 +0700

    add 1st Project

---
+++ ATM/ATM/model/Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.model
+{
+    public class Log
+    {
+        int id;
+        string code;
+        bool inOut;
+        User fromTo;
+        DateTime time;
+    }
+}
---
+++ ATM/ATM/model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.model
+{
+    public class User
+    {
+        int id;
+        string codeCredit;
+        string nameUser;
+        string passWord;
+        DateTime birthDay;
+        bool status;
+        double balance;
+    }
+}
